952b81bcb786a8c3c1304a15acd1a8e441dba3c0
Thuy Nguyen <ttruong06@gmail.com>
2022-09-22 14:14:54 -0400

    working on navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import SignupForm from './components/SignupForm';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Welcome to PetGram</h1>
+      <p>A community for animal lovers to share photos of their pet.</p>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import SigupForm from "./SignupForm";
+import Login from "./Login";
+
+import Auth from '../utils/auth'
 
 const Navbar = () => {
+    //set modal display state
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <div>
-            Navbar Page
-        </div>
+        <>
+        <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container fluid>
+                <Navbar.brand as={Link} to='/'>
+                    PetGram Logo
+                </Navbar.brand>
+                <Navbar.Toggle aria-controls='navbar' />
+                <Navbar.Collapse id='navbar'>
+                    <Nav className='ml-auto'>
+                        {/* if user is logged in, show dashboard */}
+                            {Auth.loggedIn() ? (
+                                <>
+                                <Nav.Link as={Link} to='/dashboard'>Dashboard page</Nav.Link>
+                                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                              )} 
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        {/* set modal data */}
+        <Modal size='lg' show={showModal} onHide={()}
+        </>
+        
     )
 
 }
