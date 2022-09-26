6d7f172ab2982592fb90b917c7b4d912e469621a
Thuy Nguyen <ttruong06@gmail.com>
2022-09-26 13:24:41 -0400

    centered signupForm

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -16,28 +16,28 @@ const SignupForm = () => {
     };
 
     return (
-        <>
-            <Form noValidate validated={validated} on Submit={handleSubmit}>
-                <Form.Group as={Col} md="4" className="mb-3" controlId="formBasicName">
+        <div className="loginContainer">
+            <Form className="formBox" noValidate validated={validated} on Submit={handleSubmit}>
+                <Form.Group as={Col} controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
                     <Form.Control type='text' placeholder='Your name' name='email' required/>
                     <Form.Control.Feedback type='invalid'>Full Name is required!</Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group as={Col} md="4" className="mb-3" controlId="formBasicEmail">
+                <Form.Group as={Col} controlId="formBasicEmail">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type='text' placeholder='Your email' name='email' required/>
                     <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
                 </Form.Group>
 
-                <Form.Group as={Col} md="4" className="mb-3" controlId="formBasicPasword">
+                <Form.Group as={Col} controlId="formBasicPasword" className="mb-3">
                     <Form.Label hmtlFor='password'>Password</Form.Label>
                     <Form.Control type='password' placeholder='Your password' name='password' required />
                     <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
                 </Form.Group>
                 <Button variant="primary" type="submit">Submit</Button>
             </Form>
-        </>
+        </div>
     )
 
 }
