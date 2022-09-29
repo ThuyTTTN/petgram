37878c2aca6d05d51e37972257ec87687ee92f8e
Thuy Nguyen <ttruong06@gmail.com>
2022-09-29 14:16:31 -0400

    added paw icon to the navbar section

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 
-
-
 const AppNavbar = () => {
-  return (
+
+ 
+  return ( 
     <>
     <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">PetGram</Navbar.Brand>
+          <Navbar.Brand href="/"><i class="fa-solid fa-paw"></i>  PetGram</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/dashboard">Home</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
